a74b1f7801e4a63f8f86c742dbbd364b17d3ce9a
Cody DiBella <30646495+CodyDiBella@users.noreply.github.com>
2024-01-27 02:07:30 -0600

    Update tracker.js

--- pages/tracker.js
+++ pages/tracker.js
@@ -103,12 +103,12 @@ const Tracker = () => {
       <style jsx>{`
         .bubble {
           position: absolute;
-          background-color: #333;
+          background-color: gray;
           border: 1px solid #555;
           padding: 10px;
           z-index: 1;
           border-radius: 15px;
-          color: #fff;
+          color: white;
         }
 
         .bubble ul {
