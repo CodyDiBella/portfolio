cf939fdcd23cfb343917b0095fa9f603acb3d979
Cody DiBella <30646495+CodyDiBella@users.noreply.github.com>
2024-01-27 02:27:59 -0600

    Update tracker.js

--- pages/tracker.js
+++ pages/tracker.js
@@ -1,4 +1,4 @@
- import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import trackerImg from "../public/assets/games/trackerImg.png";
 
@@ -8,6 +8,15 @@ const Tracker = () => {
   const [hourCounters, setHourCounters] = useState(Array(24).fill(0));
   const [shiftTotal, setShiftTotal] = useState(0);
   const [hoveredHour, setHoveredHour] = useState(null);
+  const [currentTime, setCurrentTime] = useState(getFormattedTime());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(getFormattedTime());
+    }, 60000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   const handleAdd = () => {
     const now = new Date();
@@ -51,6 +60,15 @@ const Tracker = () => {
     return hourCounters[currentHour];
   };
 
+   const getFormattedTime = () => {
+    const now = new Date();
+    const hours = now.getHours() % 12 || 12;
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const ampm = now.getHours() < 12 ? "AM" : "PM";
+
+    return `${hours}:${minutes} ${ampm}`;
+  };
+ 
   return (
     <div style={{ textAlign: "center", paddingTop: "80px" }}>
       <h1>New Ticket Tracking Buddy</h1>
@@ -60,6 +78,7 @@ const Tracker = () => {
           alt="Tracker Image"
           width={200}
         />
+           <p style={{ fontSize: "20px", marginLeft: "20px" }}>{currentTime}</p>
       </div>
       <div>
         <h2>Hourly Counters</h2>
